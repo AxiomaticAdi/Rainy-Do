051d4c678315e67f2cb5acf506f0e1f583f0e747
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-10-24 19:37:41 -0700

    Revert "Add audio slider"
    This reverts commit 4fa73c2ea4a9f0020e364b17a985b35e0b98ff45.

--- src/app/components/PrimaryLauncherButton.tsx
+++ src/app/components/PrimaryLauncherButton.tsx
@@ -10,7 +10,7 @@ export default function PrimaryLauncherButton({
     iconAlt,
 }: PrimaryLauncherButtonProps) {
     return (
-        <button className="btn btn-ghost btn-circle flex h-24 w-24 items-center justify-center">
+        <button className="btn btn-ghost btn-circle h-24 w-24">
             <Image src={iconSrc} alt={iconAlt} width="50" height="50" />
         </button>
     );
