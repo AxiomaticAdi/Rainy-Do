c8c91ea53b3209927649a0fd871d5a60b2ce6be1
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-10-24 19:55:11 -0700

    Slider fix

--- src/app/components/AudioPlayer.tsx
+++ src/app/components/AudioPlayer.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { useState, useEffect } from "react";
+
 import PrimaryLauncherButton from "./PrimaryLauncherButton";
+import { Slider } from "@/app/components/slider";
 
 let rainsounds: HTMLAudioElement;
 
 const AudioPlayer: React.FC = () => {
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [volume, setVolume] = useState<number>(50);
 
     useEffect(() => {
         // This will ensure the Audio object is created on the client side
         rainsounds = new Audio("/RainSounds.mp3");
+        rainsounds.volume = 0.5;
     }, []);
 
     const toggleAudio = (): void => {
@@ -20,12 +24,31 @@ const AudioPlayer: React.FC = () => {
         }
     };
 
+    const changeVolume = (value: number[]): void => {
+        const newVolume = value[0];
+        setVolume(newVolume);
+        rainsounds.volume = newVolume;
+    };
+
     return (
-        <div onClick={toggleAudio}>
-            <PrimaryLauncherButton
-                iconSrc={isPlaying ? "/button-pause.svg" : "/button-play.svg"}
-                iconAlt="play audio"
-            />
+        <div>
+            {rainsounds && (
+                <Slider
+                    defaultValue={[0.5]}
+                    min={0}
+                    max={1}
+                    step={0.1}
+                    onValueChange={changeVolume}
+                />
+            )}
+            <div onClick={toggleAudio}>
+                <PrimaryLauncherButton
+                    iconSrc={
+                        isPlaying ? "/button-pause.svg" : "/button-play.svg"
+                    }
+                    iconAlt="play audio"
+                />
+            </div>
         </div>
     );
 };
